bdfb525fe1b246b6277c4913400b802d3819233d
AyyItsHamza <105660935+AyyItsHamza@users.noreply.github.com>
2023-05-07 19:45:26 +0200

    funciona mas o menos todo

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require("express");
 const mongoose = require("mongoose");
 const userModel = require("./user_model");
 const songs_model = require("./songs_model");
-const playlist_model = require("./playlist_model");
+//const mega = require('megajs');
+const fs = require('fs');
+
+const email = process.env.email;
+const password = process.env.password;
 
 const path = require("path");
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const { response } = require('express');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan ("dev"));
 app.use(express.static('../frontend'))
@@ -37,6 +42,22 @@ function verifyToken(req, res, next) {
     });
 }
 
+// Upload file to Mega
+const uploadFile = (file, callback) => {
+  const readStream = fs.createReadStream(file.path);
+  const megaFile = storage.upload({
+    name: file.originalname,
+    size: file.size,
+    attributes: mega.FileAttributeType.TypeFile,
+  }, readStream);
+  megaFile.on('error', callback);
+  megaFile.on('complete', () => {
+    // Get the link to the uploaded file
+    const link = `https://mega.nz/file/${megaFile.downloadId}#${megaFile.key}`;
+    callback(null, link);
+  });
+};
+
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}.mongodb.net/?retryWrites=true&w=majority`),{
   useNewUrlParser: true,
   useFindAndModify: false,
@@ -50,45 +71,47 @@ db.once("open", function () {
   console.log("Connected successfully");
 });
 
-app.post("/melomuse/api/v1/login", async (request, response) => {
-    const userr = request.body.username;
-    const pass = request.body.password;
+  app.post("/melomuse/api/v1/login", async (request, response) => {
+      const userr = request.body.username;
+      const pass = request.body.password;
 
-    if(userr === "" || pass === ""){
-        response.status(406).json({msg:"Please fill in all fields"});
-    }  else { 
-        const user = await userModel.findOne({username: userr});
-        if(user === null){
-            return response.status(400).json({ msg:"User not found"});
-        } else {
-            if(bcrypt.compareSync(pass, user.password)){
-                const token = request.cookies.access_token;
-                if(token){
-                    const decoded = jwt.verify(token, process.env.SECRET);
-                    if(decoded.username === user.username){
-                        return response.status(400).json({ msg:"You are already logged in"});
-                    }
-                } else {
-                    const token = jwt.sign({username: user.username, id: user._id}, process.env.SECRET, {expiresIn: "1h"});
-                    user.token = token;
-                    response.cookie ("access_token", token,  {maxAge: 3600000, httpOnly: true})
-                    response.cookie ("username", user.username,  {maxAge: 3600000, httpOnly: true})
-                    return response.status(200).json({msg: "Login successful", jtw : token});
-                }
-            } else {
-                return response.status(400).json({msg:"Wrong password"});
-            }
-        }
-    }
-});
+      console.log(userr,pass)
+
+      if(userr === "" || pass === ""){
+          response.status(406).json({msg:"Please fill in all fields"});
+      }  else { 
+          const user = await userModel.findOne({username: userr});
+          if(user === null){
+              return response.status(400).json({ msg:"User not found"});
+          } else {
+              if(bcrypt.compareSync(pass, user.password)){
+                  const token = request.cookies.access_token;
+                  if(token){
+                      const decoded = jwt.verify(token, process.env.SECRET);
+                      if(decoded.username === user.username){
+                          return response.status(400).json({ msg:"You are already logged in"});
+                      }
+                  } else {
+                      const token = jwt.sign({username: user.username, id: user._id}, process.env.SECRET, {expiresIn: "1h"});
+                      user.token = token;
+                      response.cookie ("access_token", token,  {maxAge: 3600000, httpOnly: true})
+                      response.cookie ("username", user.username,  {maxAge: 3600000, httpOnly: true})
+                      response.cookie ("userId", user._id, {maxAge: 3600000, httpOnly: true})
+                      return response.status(200).json({msg: "Login successful", jtw : token});
+                  }
+              } else {
+                  return response.status(400).json({msg:"Wrong password"});
+              }
+          }
+      }
+  });
 
 app.post("/melomuse/api/v1/register", async (request, response) => {
     const userr = request.body.username;
-    var pass = request.body.password;
-    const nombre = request.body.nombre;
-    
-    var hashPassword = await bcrypt.hash(pass, 10);
-    pass = hashPassword;
+    const pass = request.body.password;
+    const nombre = request.body.name;
+  
+    const hashPassword = await bcrypt.hashSync(pass, 10);
 
   if(userr === "" || pass === "" || nombre === ""){
       return response.status(406).json({msg:"Please fill in all fields"});
@@ -96,9 +119,10 @@ app.post("/melomuse/api/v1/register", async (request, response) => {
       const user = await userModel.findOne({username: userr});
       if(user === null){
             const user = new userModel({
-                nombre: nombre,
+                name: nombre,
                 username: userr,
-                password: hashPassword
+                password: hashPassword,
+                playlists: [{name : "default Playlist"}]
             });
           user.save();
           return response.status(200).json({msg:"User created successfully"});
@@ -108,24 +132,23 @@ app.post("/melomuse/api/v1/register", async (request, response) => {
   }
 });
 
-app.post("/melomuse/api/v1/add_song", verifyToken, async (request, response) => {
-  var song = new songs_model({
-      username: request.cookies.username,
-      title : request.body.song_title,
-      artist : request.body.song_artist,
-      filepath : request.body.filepath,
-  });
-
-  if(song.title === "" || song.artist === "" || song.filepath === "" ){
-      return response.status(406).json({msg: "Please fill atleast the text field"});
-  } else { 
-      song.save(); 
-      return response.status(200).json({msg: "Song Uploaded successfully"});
-  } 
+app.post("/melomuse/api/v1/add_song", verifyToken, async (req, res) => {
+  try {
+    const newSong = new songs_model({
+      title: req.body.title,
+      artist: req.body.artist,
+      file_path: req.body.filepath
+    });
+    const savedSong = await newSong.save();
+    res.json(savedSong);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 
-app.get('/melomuse/api/v1/songs',async, (request, response) => {
+app.get('/melomuse/api/v1/songs', async (request, response) => {
     try{
         const songs = await songs_model.find({});
         return response.json(songs).status(200);
@@ -134,9 +157,11 @@ app.get('/melomuse/api/v1/songs',async, (request, response) => {
     }
 });
 
-app.get('/melomuse/api/v1/songs/:id',verifyToken, async (req, res) => {
+app.get('/melomuse/api/v1/song/:id',verifyToken, async (req, res) => {
+
     try {
       const song = await songs_model.findById(req.params.id);
+      console.log(song)
       if (!song) {
         return res.status(404).json({ message: 'Song not found' });
       }
@@ -147,19 +172,36 @@ app.get('/melomuse/api/v1/songs/:id',verifyToken, async (req, res) => {
     }
 });
 
-app.get('/melomuse/api/v1/playlists',verifyToken, async (req, res) => {
-    try {
-      const playlists = await playlist_model.find({});
-      res.json(playlists);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server error' });
+app.get('/melomuse/api/v1/user/:userId/playlists',verifyToken, async (req, res) => {
+  const userId = req.cookies.userId;
+
+  try {
+    // Busca el usuario en MongoDB por su ID
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'Usuario no encontrado' });
     }
+
+    // Obtiene los _id y nombres de las playlists del usuario
+    const playlists = user.playlists.map(playlist => {
+      return {
+        _id: playlist._id,
+        name: playlist.name
+      };
+    });
+
+    res.json(playlists);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error del servidor' });
+  }
 });
 
-router.get('/melomuse/api/v1/playlists/:id', verifyToken, async (req, res) => {
+app.get('/melomuse/api/v1/playlists/:id', verifyToken, async (req, res) => {
+  const playlistId = req.params.playlistId
+
     try {
-      const playlist = await playlist_model.findById(req.params.id);
+      const playlist = await userModel.findById(playlistId);
       if (!playlist) {
         return res.status(404).json({ message: 'Playlist not found' });
       }
@@ -200,7 +242,7 @@ app.delete('/melomuse/api/v1/delete/playlist/:id',verifyToken, async (req, res) 
     }
 });
 
-router.post('/melomuse/api/v1/logout', verifyToken, async (req, res) => {
+app.post('/melomuse/api/v1/logout', verifyToken, async (req, res) => {
     try {
       req.session.destroy();
       res.json({ message: 'User logged out' });
@@ -210,7 +252,7 @@ router.post('/melomuse/api/v1/logout', verifyToken, async (req, res) => {
     }
 });
 
-router.get('/melomuse/api/v1/user/:id', verifyToken, async (req, res) => {
+app.get('/melomuse/api/v1/user/:id', verifyToken, async (req, res) => {
     try {
       const user = await userModel.findById(req.params.id);
       if (!user) {
@@ -223,7 +265,7 @@ router.get('/melomuse/api/v1/user/:id', verifyToken, async (req, res) => {
     }
   });
 
-router.put('/api/v1/user/update/:id', verifyToken, async (req, res) => {
+app.put('/api/v1/user/update/:id', verifyToken, async (req, res) => {
     try {
       const user = await userModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
       if (!user) {
